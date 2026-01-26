d17dcbdf352f64b28bd3825febb8908ceee00abf
Rafael Victor <orafavictor@gmail.com>
2026-01-26 19:46:56 -0300

    feat: product routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import productsRouter from 'modules/products/routes/ProductsRoutes';
 
 const routes = Router();
 
 routes.get('/health', (request, response) => {
   return response.json({ message: 'Hello, Dev!'});
 });
+routes.use('/products', productsRouter)
 
 export default routes;
