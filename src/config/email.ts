11b92c026a21987597c506a963a45df6858fb86f
Rafael Victor <orafavictor@gmail.com>
2026-01-30 19:13:55 -0300

    feat: send email config

---
+++ src/config/email.ts
@@ -0,0 +1,33 @@
+import nodemailer from 'nodemailer';
+import 'dotenv/config';
+
+interface ISendEmail {
+  to: string;
+  subject: string;
+  body: string;
+}
+
+export const transporter = nodemailer.createTransport({
+  service: 'gmail', // ou outro serviço de email que você estiver usando
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = ({to, subject, body}: ISendEmail) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html: body,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Erro ao enviar email:', error);
+    } else {
+      console.log('Email enviado com sucesso:', info.response);
+    }
+  });
+}
