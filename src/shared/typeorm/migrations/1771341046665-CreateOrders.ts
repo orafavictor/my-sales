50bc8df647664e0dcffef40250281be5c7ea03bc
Rafael Victor <orafavictor@gmail.com>
2026-02-17 12:24:42 -0300

    feat: migration orders

---
+++ src/shared/typeorm/migrations/1771341046665-CreateOrders.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateOrders1771341046665 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ]
+      })
+    )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('orders');
+    }
+
+}
---
+++ src/shared/typeorm/migrations/1771341464984-AddCustomerIdToOrders.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
+
+export class AddCustomerIdToOrders1771341464984 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn(
+        'orders',
+        new TableColumn({
+          name: 'customer_id',
+          type: 'integer',
+          isNullable: true,
+        }),
+      ),
+
+      await queryRunner.createForeignKey(
+        'orders',
+        new TableForeignKey({
+          name: 'OrdersCustomer',
+          columnNames: ['customer_id'],
+          referencedTableName: 'customers',
+          referencedColumnNames: ['id'],
+          onDelete: 'SET NULL',
+        }),
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
